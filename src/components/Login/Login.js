7a79e1bc25e6fec6be2a67f66c9dcc2f18936126
Wavhudi Cornelius Mulaudzi <corneliusmulaudzi@gmail.com>
2021-02-05 09:15:08 +0200

    Login authentication is working now the weather page is accesible only by authenticated users there's room for improvements here but this will do for now

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { authentication } from '../../firebase/Methods';
+
+// importing components
+import Weather from '../Weather/Weather';
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is the login screen</h1>
-        </div>
-    )
+
+    // states to store cellphone number
+    const [pnumber, setPnumber] = useState('');
+    const [password, setPassword] = useState('');
+    const [authStatus, setAuthStatus] = useState(false);
+
+    const auth = async () => {
+        const res = await authentication(pnumber, password);
+        setAuthStatus(res);
+    }
+    
+    if(authStatus) {
+        return (
+            <Weather />
+        )
+    }else {
+        return (
+            <div className="login-container">
+                <input type="text" className="pnumber" placeholder="Phone Number" value={pnumber} onChange={(e) => setPnumber(e.target.value)} />
+                <input type="text" className="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={auth}>Log In Now</button>
+            </div>
+        )
+    }
 }
 
 export default Login
--- src/firebase/Methods.js
+++ src/firebase/Methods.js
@@ -19,3 +19,23 @@ export const addAccount = (name, surname, pnumber, email, password ) => {
         return false;
     });
 }
+
+// user authentication
+// checks if user phonenumber and password are vaild and that they are in the database
+// it returns true or false based on the respond
+export const authentication = (pnumber, password) => {
+
+    return accountsRef.get().then((querySnapshot) => {
+        const docs = querySnapshot.docs;
+        var res = false;
+        for(let i = 0; i < docs.length; i++) {
+            if(docs[i].id === pnumber) {
+                if(docs[i].data().password === password) {
+                    res = true;
+                    break;
+                }
+            }
+        }
+        return res;
+    });
+}
